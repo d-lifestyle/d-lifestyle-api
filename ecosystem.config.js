f0f93ad4d437b77af82464e979d0849a1d965326
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-04-19 12:29:33 +0530

    change script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
      apps: [
           {
                name: "d-lifestyle-api",
-               cwd: "src/server.ts",
+               cwd: "cross-env DEBUG=app:* nodemon --watch 'src/**/*' -e ts --exec ./node_modules/.bin/ts-node -r tsconfig-paths/register ./src/bin/www.ts",
                script: "npm",
                args: "start",
                env: {
