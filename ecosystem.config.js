cb8bc2b75117a4322a7673760eee921c3907a5de
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-04-19 12:11:19 +0530

    added config file

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+     apps: [
+          {
+               name: "d-lifestyle-api",
+               cwd: "./src/server.ts",
+               script: "npm",
+               args: "start",
+               env: {
+                    NODE_ENV: "production",
+               },
+          },
+     ],
+};
