06a16ac8d417c680d26dff713514bae43fc6b3f4
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-04-19 12:55:34 +0530

    change script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
      apps: [
           {
                name: "d-lifestyle-api",
-               script: "npm run start", // path needs to be relative from ecosystem.config.js
+               script: "npm start", // path needs to be relative from ecosystem.config.js
                watch: true, // any changes to app folder will get pm2 to restart app
                env: {
                     DB_PATH: "mongodb+srv://dinesh:hgfUYWbjVCheedBd@cluster0.hoqxjop.mongodb.net/?retryWrites=true&w=majority",
