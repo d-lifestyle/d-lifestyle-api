c64f94a3c2bdc5e22923f1bc0b6c7ba1853343b2
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-04-19 12:45:09 +0530

    update config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
      apps: [
           {
-               name: "d-lifestyle-api",
-               cwd: "/root/dev/zhaogong-be",
-               script: "npm",
-               args: "start",
+               name: "my-app",
+               script: "npm run start", // path needs to be relative from ecosystem.config.js
+               watch: true, // any changes to app folder will get pm2 to restart app
                env: {
-                    NODE_ENV: "production",
+                    NODE_ENV: "production", // define env variables here
+                    DB_PATH: "mongodb+srv://dinesh:hgfUYWbjVCheedBd@cluster0.hoqxjop.mongodb.net/?retryWrites=true&w=majority",
                },
           },
      ],
